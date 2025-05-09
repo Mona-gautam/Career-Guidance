b2d2503c06587016d498b07c9c723cf15831db6b
mona-gautam <monagautam489@gmail.com>
2025-05-09 11:41:21 +0530

    third commit

--- ai-career-coach-master/app/(main)/interview/layout.js
+++ ai-career-coach-master/app/(main)/interview/layout.js
@@ -1,8 +1,8 @@
-import { Suspense } from "react";
+ { Suspense } from "react";
 import { BarLoader } from "react-spinners";
 
 export default function Layout({ children }) {
-  return (
+  retuimportrn (
     <div className="px-5">
       <Suspense
         fallback={<BarLoader className="mt-4" width={"100%"} color="gray" />}
